6cc7b9214efac94fff47000e0e7696bdfc0b6173
W.Bielecki <bva@sibdigital.net>
2022-03-18 10:58:26 +0800

    change button color

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,18 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-
-@Composable
-@Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Column(modifier = Modifier.fillMaxWidth(),) {
-            Button(onClick = {}){
-                Text(text = "WebCam")
-            }
-        }
-    }
-}
-
 fun main() = application {
 
     Window(
@@ -38,3 +22,20 @@ fun main() = application {
         App()
     }
 }
+@Composable
+@Preview
+fun App() {
+    var text by remember { mutableStateOf("Hello, World!") }
+
+    MaterialTheme {
+        Column(modifier = Modifier.fillMaxWidth(),) {
+            Button(onClick = {},
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Green)
+                ){
+                Text(text = "WebCam")
+            }
+        }
+    }
+}
+
+
